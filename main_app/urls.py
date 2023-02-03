c0634263e89d168a85a5ce0a01d0a34b188a8c22
xandramiyo <aj.sumiyoshi@gmail.com>
2023-02-03 14:41:53 -0800

    basics of part 1 complete. no styling

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name='home'),
+	path('about/', views.about, name='about'),
+	path('pokemon/', views.pokemon_index, name='index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+pokemon = [
+	{'name': 'Rowlet', 'type': ['Grass'], 'ability': 'Overgrow', 'level': 5},
+	{'name': 'Toxtricity', 'type': ['Poison', 'Electric'], 'ability': 'Plus', 'level': 30}
+]
 
 # Create your views here.
+def home(request):
+	return render(request, 'home.html')
+
+def about(request):
+	return render(request, 'about.html')
+
+def pokemon_index(request):
+	return render(request, 'pokemon/index.html')
